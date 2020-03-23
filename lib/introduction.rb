f256120b5b53182ec3964c9e9fb4e68f8c5eef20
anthony mcmullen <amcmullen9291@gmail.com>
2020-03-23 13:38:37 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,7 +3,14 @@ def introduction(name)
 end
 
 def introduction_with_language(name,language)
+<<<<<<< HEAD
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 introduction_with_language("Dan","Ember.js")
+=======
+  puts `"Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+introduction_with_language("Dan", "Ember.js")
+>>>>>>> d421d49090d4f7e3b1857dd78dce755989bfb524
